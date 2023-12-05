20f4e2d1b07d7baddcd29157b495041e4519304e
jwlebert <j.lebert4402@gmail.com>
2023-12-05 16:00:56 -0500

    2023 d5p2 - added benchmarking to rust program

--- 2023/day5/var/part2/src/main.rs
+++ 2023/day5/var/part2/src/main.rs
@@ -2,6 +2,7 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 
+use std::time::Instant;
 use std::fs::File;
 use std::io::Read;
 
@@ -11,19 +12,11 @@ struct Input {
     maps: Vec<Vec<(u64, u64, u64)>>,
 }
 
-fn main() {
-    let mut file = File::open("input.json").unwrap();
-    let mut input = String::new();
-    file.read_to_string(&mut input).unwrap();
-
-    let json: Input =
-        serde_json::from_str(&input).unwrap();
-
+fn map_seeds(inp: Input) -> Vec<u64> {
     let mut mapped_seeds: Vec<u64> = Vec::new();
-
-    for range in json.seed_ranges {
+    for range in inp.seed_ranges {
         for mut seed in range.0..range.1 {
-            for map in &json.maps {
+            for map in &inp.maps {
                 for (dest, src, length) in map {
                     if (src..&((src + length))).contains(&&seed) {
                         seed = dest + seed - src;
@@ -34,10 +27,26 @@ fn main() {
             mapped_seeds.push(seed);
         }
     }
+    mapped_seeds
+}
+
+fn main() {
+    let mut file = File::open("input.json").unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+
+    let json: Input =
+        serde_json::from_str(&input).unwrap();
+
+    let now = Instant::now();
+    let mapped_seeds: Vec<u64> = map_seeds(json);
 
     let lowest_distance = mapped_seeds.iter().min();
     match lowest_distance {
         Some(min) => println!( "Lowest distance is {}", min ),
         None      => println!( "Vector is empty" ),
     }
+
+    let elapsed = now.elapsed();
+    println!("Time spent: {:.2?}", elapsed);
 }
